051824287ab9f750aedc85b3050d6064fb71bc4e
jjeanjacques10 <jjean.jacques10@gmail.com>
2024-04-28 01:25:27 -0300

    (feature): add models to consumer and update application

--- notification/src/main/kotlin/com/payments/notification/adapter/input/kafka/PaymentKafkaConsumer.kt
+++ notification/src/main/kotlin/com/payments/notification/adapter/input/kafka/PaymentKafkaConsumer.kt
@@ -1,6 +1,10 @@
 package com.payments.notification.adapter.input.kafka
 
 import com.payment.TransactionItem
+import com.payments.notification.domain.enums.KeyType
+import com.payments.notification.domain.model.Destination
+import com.payments.notification.domain.model.Origin
+import com.payments.notification.domain.model.Transaction
 import com.payments.notification.domain.service.NotificationService
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
@@ -9,6 +13,8 @@ import org.springframework.kafka.support.KafkaHeaders
 import org.springframework.messaging.handler.annotation.Header
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Component
+import java.math.BigDecimal
+import java.time.LocalDateTime
 
 @Component
 class PaymentKafkaConsumer(
@@ -17,7 +23,7 @@ class PaymentKafkaConsumer(
 
     @KafkaListener(topics = ["payment-topic"], containerFactory = "kafkaListenerContainerFactory")
     fun consumePayment(
-        @Payload transaction: TransactionItem,
+        @Payload transactionItem: TransactionItem,
         @Header(KafkaHeaders.OFFSET) offset: Long,
         @Header(KafkaHeaders.RECEIVED_PARTITION) partition: Int?,
         @Header(KafkaHeaders.RECEIVED_TOPIC) topic: String?,
@@ -25,13 +31,20 @@ class PaymentKafkaConsumer(
     ) {
         try {
             log.info("Consume Kafka message - topic: {}, offset: {}, partition: {}", topic, offset, partition)
-            // notificationService.process(transaction)
+            notificationService.process(transactionItem.toTransaction())
         } catch (ex: Exception) {
             log.error("Error processing message: {}", ex.message, ex)
         } finally {
             ack.acknowledge()
         }
     }
+
+    private fun TransactionItem.toTransaction() = Transaction(
+        BigDecimal(this.value.toString()),
+        Origin(this.origin.agency.toString(), this.origin.account.toString(), this.origin.dac.toString()),
+        Destination(this.destination.key.toString(), KeyType.valueOf(this.destination.keyType.toString())),
+        LocalDateTime.parse(this.createdAt.toString())
+    )
 
     private val log = LoggerFactory.getLogger(PaymentKafkaConsumer::class.java)
 }
--- notification/src/main/kotlin/com/payments/notification/adapter/output/email/EmailNotifier.kt
+++ notification/src/main/kotlin/com/payments/notification/adapter/output/email/EmailNotifier.kt
@@ -2,11 +2,14 @@ package com.payments.notification.adapter.output.email
 
 import com.payments.notification.domain.model.Transaction
 import com.payments.notification.domain.port.output.NotificationProcessor
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
 class EmailNotifier : NotificationProcessor {
     override fun notify(transation: Transaction) {
-        TODO("Not yet implemented")
+        log.info("Email sent to ${transation.origin.agency} ${transation.origin.account}-${transation.origin.dac}")
     }
+
+    private val log = LoggerFactory.getLogger(EmailNotifier::class.java)
 }
